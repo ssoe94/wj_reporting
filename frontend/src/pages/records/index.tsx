db14457e266161b13032c7f5d07a40da318c267b
Jaeyoung Lee <ssoe94@gmail.com>
2025-06-18 11:36:47 +0800

    chore: React 17+ 환경에 맞게 불필요한 import React 제거 및 코드 정리

--- frontend/src/pages/records/index.tsx
+++ frontend/src/pages/records/index.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
 
 interface InjectionRecord {
   id: number;
